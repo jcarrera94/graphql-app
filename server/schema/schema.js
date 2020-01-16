8b9c395f5ee85da826b1b1cb686b258c610a766e
Juan Carrera <juankarrera@gmail.com>
2020-01-15 22:10:31 -0500

    add list of books in author type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,41 +1,57 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { 
-  GraphQLObjectType, 
-  GraphQLString, 
+const {
+  GraphQLObjectType,
+  GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
-  } = graphql;
+  GraphQLInt,
+  GraphQLList
+} = graphql;
 
 const books = [
-  { name: 'abc', genre: 'educational', id: '1' },
-  { name: 'Javascript 101', genre: 'educational', id: '2' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '3' },
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
 const authors = [
-  { name: 'Juan Carrera', age: 25, id: '1' },
-  { name: 'Joseph Stimac', age: 55, id: '2' },
-  { name: 'Lauren Phillips', age: 38, id: '3' },
-]
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' }
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString  }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId })
+      }
+    }
   })
 });
 
-const AuthorType = new GraphQLObjectType ({
+const AuthorType = new GraphQLObjectType({
   name: 'Author',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+       type: new GraphQLList(BookType),
+       resolve(parent, args) {
+         return _.filter(books, { authorId: parent.id })
+       }
+    }
   })
 })
 
