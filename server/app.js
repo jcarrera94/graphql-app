e041a219b06a5de751d36d1f81c652e1c46284e5
Juan Carrera <juankarrera@gmail.com>
2020-01-15 23:23:09 -0500

    unsucessful attempt to connect to mongodb with mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://jcarrera94:Abcd0192837465@jcarrera94-ctzdh.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => console.log("connected to db"))
+.catch(err => console.error(err.message));
 
 app.use("/graphql", graphqlHTTP({
   schema,
